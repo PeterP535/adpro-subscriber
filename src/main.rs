6dec2e4d9c4289f8f25419b31b50f2b7b8039813
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2025-05-16 18:18:40 +0700

    Initial subscriber code.

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+use std::{thread, time};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        let ten_millis = time::Duration::from_millis(1000);
+        thread::sleep(ten_millis);
+        println!("In Peter's Computer [1295004y]. Message received: {:?}", message);
+        Ok(())
+    }
+        fn get_handler_action(&self) -> String {
+        todo!()
+    }
+}
+
+
+fn main() {
+    let listener =
+CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = listener.listen("user_created".to_owned(), UserCreatedHandler{},
+crosstown_bus::QueueProperties { auto_delete: false, durable: false,
+use_dead_letter: true });
+    loop {
+}
+}
